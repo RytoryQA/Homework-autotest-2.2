f764ed2a92ebefb840c9312dd6de3410ab5aaa51
RytoryQA <vikaryzhkina89@gmail.com>
2023-10-25 11:47:09 +0400

    check appveyor: returning tests to their original state

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
-import static sun.text.normalizer.NormalizerImpl.UTF16Plus.equal;
 
 public class PostmanEchoTest {
 
@@ -20,7 +19,7 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equal("Привет из Постмана"))
+                .body("data", equalTo("Привет из Постмана"))
         ;
 
     }
