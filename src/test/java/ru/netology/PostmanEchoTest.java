8fd3075e7d894054d8d97e844c598d47166becb1
RytoryQA <vikaryzhkina89@gmail.com>
2023-10-25 11:35:10 +0400

    check appveyor

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -19,7 +19,7 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Привет из Постмана"))
+                .body("data", equals("Привет из Постмана"))
         ;
 
     }
